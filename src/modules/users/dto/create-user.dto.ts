f7733ea1561dd60e984e2ee83ac60d4f74ae36b0
Minh Trần <tranminhbvhp@gmail.com>
2024-09-17 16:50:11 +0700

    FEAT (user): on basing

--- src/modules/users/dto/create-user.dto.ts
+++ src/modules/users/dto/create-user.dto.ts
@@ -1,1 +1,60 @@
-export class CreateUserDto {}
+import {
+  IsEmail,
+  IsNotEmpty,
+  IsNumber,
+  IsOptional,
+  IsStrongPassword,
+} from 'class-validator';
+import {
+  ADDRESS_MUST_NOT_EMPTY,
+  EMAIL_MUST_NOT_EMPTY,
+  EMAIL_MUST_VALID,
+  NAME_MUST_NOT_EMPTY,
+  PASSWORD_MUST_NOT_EMPTY,
+  PASSWORD_NOT_STRONG,
+  ROLE_MUST_NOT_EMPTY,
+  ROLE_ID_MUST_NUMBER,
+  COMPANY_ID_MUST_NUMBER,
+  COMPANY_MUST_NOT_EMPTY,
+} from 'src/modules/utils/messageConstants';
+
+export class CreateUserDto {
+  @IsEmail({}, { message: EMAIL_MUST_VALID })
+  @IsNotEmpty({ message: EMAIL_MUST_NOT_EMPTY })
+  email: string;
+
+  @IsStrongPassword(
+    {
+      minLength: 6,
+      minUppercase: 1,
+      minLowercase: 1,
+      minNumbers: 1,
+      minSymbols: 1,
+    },
+    {
+      message: PASSWORD_NOT_STRONG,
+    },
+  )
+  @IsNotEmpty({ message: PASSWORD_MUST_NOT_EMPTY })
+  password: string;
+
+  @IsNotEmpty({ message: NAME_MUST_NOT_EMPTY })
+  name: string;
+
+  @IsNotEmpty({ message: NAME_MUST_NOT_EMPTY })
+  genderId: string;
+
+  @IsNotEmpty({ message: NAME_MUST_NOT_EMPTY })
+  phoneNumber: string;
+
+  @IsNotEmpty({ message: ADDRESS_MUST_NOT_EMPTY })
+  address: string;
+
+  @IsNumber({}, { message: ROLE_ID_MUST_NUMBER })
+  @IsNotEmpty({ message: ROLE_MUST_NOT_EMPTY })
+  roleId: string;
+
+  @IsNumber({}, { message: COMPANY_ID_MUST_NUMBER })
+  @IsNotEmpty({ message: COMPANY_MUST_NOT_EMPTY })
+  companyId: string;
+}
