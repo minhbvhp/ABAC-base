ac0822cf445c254535fc651d7ac4c77bae2c0446
Minh Trần <tranminhbvhp@gmail.com>
2024-10-02 11:20:04 +0700

    FEAT (jwt strategy): get token from environment variable

--- src/modules/auth/strategies/jwt-access-token.strategy.ts
+++ src/modules/auth/strategies/jwt-access-token.strategy.ts
@@ -1,5 +1,4 @@
 import { Injectable } from '@nestjs/common';
-import { ConfigService } from '@nestjs/config';
 import { PassportStrategy } from '@nestjs/passport';
 import { ExtractJwt, Strategy } from 'passport-jwt';
 import { TokenPayload } from '../interfaces/token.interface';
@@ -11,7 +10,7 @@ export class JwtAccessTokenStrategy extends PassportStrategy(Strategy) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       ignoreExpiration: false,
-      secretOrKey: 'access_token_secret',
+      secretOrKey: process.env.JWT_ACCESS_TOKEN_SECRET,
     });
   }
 
