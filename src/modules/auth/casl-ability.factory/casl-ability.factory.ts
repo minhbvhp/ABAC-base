9506a26a01476579f405828013022f1d1d4fd126
Minh <tranminhbvhp@gmail.com>
2024-10-19 21:33:47 +0700

    FEAT (casl): on basing

--- src/modules/auth/casl-ability.factory/casl-ability.factory.ts
+++ src/modules/auth/casl-ability.factory/casl-ability.factory.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@nestjs/common';
 import { AuthService } from '../auth.service';
 import User from '../../users/entities/user.entity';
 import Permission from '../../permissions/entities/permission.entity';
+import { PermissionCondition } from '../../permissions/interfaces/permissionCondition.interface';
 
 export enum PermissionAction {
   CREATE = 'create',
@@ -16,18 +17,22 @@ export type AppAbility = Ability<[PermissionAction, PermissionSubjectType]>;
 interface CaslPermission {
   action: PermissionAction;
   subject: string;
+  condition?: PermissionCondition;
 }
 
 @Injectable()
 export class CaslAbilityFactory {
   //   constructor(private authService: AuthService) {}
+
   async createForUser(user: User): Promise<AppAbility> {
     const dbPermissions: Permission[] = [];
     //   await this.authService.getAllPermissionsOfUser(user);
+
     const caslPermissions: CaslPermission[] = dbPermissions.map(
       (permission) => ({
         action: permission.action,
         subject: permission.subject.name,
+        conditions: Permission.parseCondition(permission.condition, user),
       }),
     );
     return new Ability<[PermissionAction, PermissionSubjectType]>(
--- src/modules/permissions/entities/permission.entity.ts
+++ src/modules/permissions/entities/permission.entity.ts
@@ -9,6 +9,7 @@ import {
 import Role from '../../roles/entities/role.entity';
 import Subject from '../../subjects/entities/subject.entity';
 import { PermissionAction } from '../../auth/casl-ability.factory/casl-ability.factory';
+import { PermissionCondition } from '../interfaces/permissionCondition.interface';
 
 @Entity()
 class Permission {
@@ -22,8 +23,47 @@ class Permission {
   @JoinColumn({ name: 'subject_id' })
   subject: Subject;
 
+  @Column({ type: 'json' })
+  condition: PermissionCondition;
+
   @ManyToMany(() => Role, (role) => role.permissions)
   roles: Role[];
+
+  /**
+   * @param condition: {"userId": "${id}"}
+   * @param variables: {"id: 1"}
+   * @return condition after parse: {"userId": 1}
+   */
+  public static parseCondition(
+    condition: PermissionCondition,
+    variables: Record<string, any>,
+  ): PermissionCondition {
+    if (!condition) return null;
+    const parsedCondition = {};
+    for (const [key, rawValue] of Object.entries(condition)) {
+      if (rawValue !== null && typeof rawValue === 'object') {
+        const value = this.parseCondition(rawValue, variables);
+        parsedCondition[key] = value;
+        continue;
+      }
+      if (typeof rawValue !== 'string') {
+        parsedCondition[key] = rawValue;
+        continue;
+      }
+      // find placeholder "${}""
+      const matches = /^\\${([a-zA-Z0-9]+)}$/.exec(rawValue);
+      if (!matches) {
+        parsedCondition[key] = rawValue;
+        continue;
+      }
+      const value = variables[matches[1]];
+      if (typeof value === 'undefined') {
+        throw new ReferenceError(`Variable ${name} is not defined`);
+      }
+      parsedCondition[key] = value;
+    }
+    return parsedCondition;
+  }
 }
 
 export default Permission;
