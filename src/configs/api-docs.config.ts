bc747373571b0e7c6b228192e075f67ebb36e77c
Minh Trần <tranminhbvhp@gmail.com>
2024-10-30 15:47:02 +0700

    CONFIG (readme): content README.md

--- src/configs/api-docs.config.ts
+++ src/configs/api-docs.config.ts
@@ -67,6 +67,6 @@ export function configSwagger(app: INestApplication) {
   SwaggerModule.setup('api-docs', app, document, {
     swaggerOptions: { persistAuthorization: true },
     customJs: '/swagger-custom.js',
-    jsonDocumentUrl: 'api-docs/json',
+    yamlDocumentUrl: 'api-docs/yaml',
   });
 }
